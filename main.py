a2f6863720c15bb2b133b7b2160ad1ca246fdcc0
son.luongngoc <son.luongngoc@lazada.com>
2017-08-14 20:16:35 +0700

    Added multiple domains test

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import csv
 URL_LAZADA_VN = "http://www.lazada.vn"
 URL_LAZADA_SG = "http://www.lazada.sg"
 URL_LAZADA_MY = "http://www.lazada.com.my"
+URL_LAZADA_PH = "http://www.lazada.com.ph"
 URL_LAZADA_TH = "http://www.lazada.co.th"
-URL_LAZADA_PH = "http://www.lazada.co.ph"
 URL_LAZADA_ID = "http://www.lazada.co.id"
 
-
+URL_ALL_DOMAINS = [
+    URL_LAZADA_VN,
+    URL_LAZADA_SG,
+    URL_LAZADA_MY,
+    URL_LAZADA_PH,
+    URL_LAZADA_TH,
+    URL_LAZADA_ID
+]
 
 BROWSER = webdriver.Chrome()
-BROWSER.get(URL_LAZADA_VN)
+
+for current_domain in URL_ALL_DOMAINS:
+    BROWSER.get(current_domain)
 
 BROWSER.quit()
